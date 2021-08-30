4564de00930ed8affaa544991260875b35756f29
Tanya Slavska <tslavska01@gmail.com>
2021-08-30 12:54:14 +0300

    Required added

--- src/cljs/form/components/reusable/question_item/core.cljs
+++ src/cljs/form/components/reusable/question_item/core.cljs
@@ -3,10 +3,9 @@
             [form.components.reusable.free-text.core :refer [free_text]]
             [form.components.reusable.single-choice.core :refer [single_choice]]
             [form.components.reusable.multiple-choice.core :refer [multiple_choice]]
-            [form.components.reusable.dropdown-list.core :refer [dropdown_list]]
-            [form.components.reusable.linear-scale.core :refer [linear_scale]]))
+            [form.components.reusable.dropdown-list.core :refer [dropdown_list]]))
 
-(defn type-check [type]
+(defn type-without-values-check [type]
   (condp = type
     "free-text"            free_text
     "date"                 date))
@@ -15,15 +14,16 @@
   (condp = type
     "single-choice"        single_choice
     "multiple-choice"      multiple_choice
-    "dropdown-list"        dropdown_list
-    "linear-scale"         linear_scale))
+    "dropdown-list"        dropdown_list))
 
-(defn question_item [{:keys [id type question values]}]
+(defn question_item [{:keys [id type question values required]}]
   (if values
      [(type-with-value-check type)
       {:id id
        :question question
-       :values values}]
-     [(type-check type)
+       :values values
+       :required required}]
+     [(type-without-values-check type)
       {:id id
-       :question question}]))
+       :question question
+       :required required}]))
